6d090aee06095cba43033c129480bb66e66433f0
Paul Anderson <pauleanderson@gmail.com>
2021-09-23 08:46:29 -0700

    update

--- tests/test_Topic1.py
+++ tests/test_Topic1.py
@@ -10,6 +10,12 @@ DIR=pathlib.Path(__file__).parent.absolute()
 import joblib
 answers = joblib.load(str(DIR)+"/answers_Topic1.joblib")
 
+import pandas as pd
+data = pd.read_table("http://bioinformaticsalgorithms.com/data/realdatasets/Rearrangements/E_coli.txt",header=None)
+genome = data.values[0,0]
+
+text = "atcaatgatcaacgtaagcttctaagcatgatcaaggtgctcacacagtttatccacaacctgagtggatgacatcaagataggtcgttgtatctccttcctctcgtactctcatgaccacggaaagatgatcaagagaggatgatttcttggccatatcgcaatgaatacttgtgacttgtgcttccaattgacatcttcagcgccatattgcgctggccaaggtgacggagcgggattacgaaagcatgatcatggctgttgttctgtttatcttgttttgactgagacttgttaggatagacggtttttcatcactgactagccaaagccttactctgcctgacatcgaccgtaaattgataatgaatttacatgcttccgcgacgatttacctcttgatcatcgatccgattgaagatcttcaattgttaattctcttgcctcgactcatagccatgatgagctcttgatcatgtttccttaaccctctattttttacggaagaatgatcaagctgctgctcttgatcatcgtttc"
+
 def test_exercise_1():
     assert Topic1_helper.count("ACAACTATGCATACTATCGGGAACTATCCT","ACTAT") == answers['answer_exercise_1a']
     
@@ -23,10 +29,10 @@ def test_exercise_3():
     assert Topic1_helper.reverse_complement("cagt") == answers['answer_exercise_3']
     
 def test_exercise_4():
-    assert Topic1_helper.frequency_table(Lab1.text,3) == answers["answer_exercise_4"]
+    assert Topic1_helper.frequency_table(Topic1_helper.text,3) == answers["answer_exercise_4"]
 
 def test_exercise_5():
-    assert Topic1_helper.better_frequent_words(Lab1.text,9) == answers["answer_exercise_5"]
+    assert Topic1_helper.better_frequent_words(Topic1_helper.text,9) == answers["answer_exercise_5"]
     
 def test_exercise_6():
-    assert Topic1_helper.skew(Lab1.genome) == answers["answer_exercise_6"]
+    assert Topic1_helper.skew(Topic1_helper.genome) == answers["answer_exercise_6"]
